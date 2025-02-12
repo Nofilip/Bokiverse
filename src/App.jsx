0178290a7a61d80d747c95f9822d26d1bde5e4f9
Filip Norgren <nf7_@hotmail.com>
2025-02-12 10:37:26 +0100

    Setting up project from start

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
   return (
-    <div className="min-h-screen flex justify-center items-center bg-gray-100">
-      <button className="px-4 py-2 bg-blue-500 text-white rounded-lg">
-        Klicka här
-      </button>
-    </div>
+    <>
+    
+    </>
   );
 }
 
