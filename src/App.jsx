d6697309344d3ec5fa9a0e04ed3f5da540513cc8
Filip Norgren <nf7_@hotmail.com>
2025-02-17 15:58:12 +0100

    initial commit for productgrid

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
 import './App.css'
 import Header from './components/header/header';
 import NavBar from './components/NavBar/NavBar';
+import ProductGrid from './components/Products/ProductGrid';
+
 
 
 function App() {
+
+  const Products = [];
+
+  fetch("http://localhost:8000/api/tasks")
+    .then(resp => resp.json())
+    .then(Products => {
+      console.log(Products);
+    })
+  
   return (
     <>
       <Header />
 
       <NavBar />
+
+      <ProductGrid Products={Products} />
     </>
   );
 }
---
+++ src/components/Products/ProductGrid.jsx
@@ -0,0 +1,9 @@
+const ProductGrid = () => {
+  return (
+    <section>
+      <div><img src="https://placehold.co/200x300" alt="" /></div>
+    </section>
+  )
+}
+
+export default ProductGrid;
