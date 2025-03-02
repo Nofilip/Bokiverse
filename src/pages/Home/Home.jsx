2adaac66556cfe97815ec164864e6ca1548c4510
Filip Norgren <nf7_@hotmail.com>
2025-03-02 07:04:18 +0100

    Set up router

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from 'react';
+
+import Hero from '../../components/Hero/Hero';
+import Header from '../../components/Header/Header';
+import NavBar from '../../components/NavBar/NavBar';
+import ProductGrid from '../../components/Products/ProductGrid';
+import HiddenSection from '../../components/HiddenSection/HiddenSection';
+
+function Home() {
+
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/api/tasks")
+      .then(resp => resp.json())
+      .then(data => {
+        setProducts(data);
+      })
+      
+  }, []);
+  
+  return (
+    <>
+      <Header />
+   
+      <NavBar />
+
+      <Hero />
+
+      <HiddenSection />
+
+      <ProductGrid products={products} />
+
+      
+
+    </>
+  );
+}
+
+export default Home;
---
+++ src/pages/ProductDetails/ProductDetails.jsx
@@ -0,0 +1,9 @@
+function ProductDetails() {
+  return(
+    <h1></h1>
+  )
+
+
+}
+
+export default ProductDetails;
